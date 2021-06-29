3b7c3c2eb9d61e2a2008d9d43d6c20b3fc950f03
joalon <joakimlonnegren@gmail.com>
2021-06-29 19:37:20 +0200

    Getting started with unit tests. Adds an assert true using busted. Seems to work pretty well!

---
+++ spec/test_spec.lua
@@ -0,0 +1,9 @@
+require 'busted'
+
+describe("The neovim stgit plugins test suite", function()
+  describe("should be run always", function()
+    it("and it should be easy to use", function()
+      assert.truthy("Sure is!")
+    end)
+  end)
+end)
